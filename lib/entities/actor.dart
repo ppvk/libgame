30246ef80d1fe254ec2789d2136353df1ed6b341
ppvk <paul.vankeuren@gmail.com>
2015-04-17 22:45:49 -0500

    expose Actor's entity

--- lib/entities/actor.dart
+++ lib/entities/actor.dart
@@ -3,15 +3,15 @@ part of libgame;
 // convenience wrapper class.
 class Actor {
 
-  Actor(this._entity) {
+  Actor(this.entity) {
     print('constructed');
   }
-  Entity _entity;
+  Entity entity;
 
-  PositionComponent get position => _entity.getComponentByClass(PositionComponent);
-  VelocityComponent get velocity => _entity.getComponentByClass(VelocityComponent);
-  AccelerationComponent get acceleration => _entity.getComponentByClass(AccelerationComponent);
-  ForceComponent get force => _entity.getComponentByClass(ForceComponent);
-  MassComponent get mass => _entity.getComponentByClass(MassComponent);
-  SpriteComponent get sprite => _entity.getComponentByClass(SpriteComponent);
+  PositionComponent get position => entity.getComponentByClass(PositionComponent);
+  VelocityComponent get velocity => entity.getComponentByClass(VelocityComponent);
+  AccelerationComponent get acceleration => entity.getComponentByClass(AccelerationComponent);
+  ForceComponent get force => entity.getComponentByClass(ForceComponent);
+  MassComponent get mass => entity.getComponentByClass(MassComponent);
+  SpriteComponent get sprite => entity.getComponentByClass(SpriteComponent);
 }
