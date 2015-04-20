5c1d081eaf940d70bf0b455e7c5bbe07e416e528
Robert McDermot <robert.mcdermot@gmail.com>
2015-04-20 09:11:44 -0400

    fix bug: Uncaught Error: type 'WhereIterable' is not a subtype of type 'List<Entity>' of 'entities'

--- lib/entities/room.dart
+++ lib/entities/room.dart
@@ -1,27 +1,26 @@
 part of libgame;
 
 // convenience wrapper class.
-class Room {
+class Room
+{
+	Entity entity;
 
-  Room(this.entity);
-  Entity entity;
+	Room(this.entity);
 
-  DisplayContainerComponent get display => entity.getComponentByClass(DisplayContainerComponent);
+	DisplayContainerComponent get display => entity.getComponentByClass(DisplayContainerComponent);
 
-  Bag get actorEntities => (entity.getComponentByClass(EntityBagComponent) as EntityBagComponent).entities;
+	Bag get actorEntities => (entity.getComponentByClass(EntityBagComponent) as EntityBagComponent).entities;
 
-  Actor getActorByName(String name) {
-    Entity actorEntity = actorEntities.singleWhere((Entity e)
-      => (e.getComponentByClass(MetaComponent) as MetaComponent).meta['name'] == name);
-    return new Actor(actorEntity);
-  }
+	Actor getActorByName(String name) {
+		Entity actorEntity = actorEntities.singleWhere((Entity e) => (e.getComponentByClass(MetaComponent) as MetaComponent).meta['name'] == name);
+		return new Actor(actorEntity);
+	}
 
-  List <Actor> getActorsByTag(String tag) {
-    List <Entity> entities = actorEntities.where((Entity e)
-    => (e.getComponentByClass(MetaComponent) as MetaComponent).meta['tags'].contains(tag));
-    List <Actor> actors = [];
-    for (Entity e in entities)
-      actors.add(new Actor(e));
-    return actors;
-  }
+	List<Actor> getActorsByTag(String tag) {
+		List<Entity> entities = actorEntities.where((Entity e) => (e.getComponentByClass(MetaComponent) as MetaComponent).meta['tags'].contains(tag));
+		List <Actor> actors = [];
+		for(Entity e in entities)
+			actors.add(new Actor(e));
+		return actors;
+	}
 }
